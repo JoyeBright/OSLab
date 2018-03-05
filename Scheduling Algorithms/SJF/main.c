42a777ba881be13b6d26db7adc7ae163f4178e05
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-03-05 04:58:12 +0330

    Now SJF is working rightly

--- Scheduling Algorithms/SJF/main.c
+++ Scheduling Algorithms/SJF/main.c
@@ -2,7 +2,8 @@
 
 int main() {
   /* code */
-  int i,j,n,bt[20],pos,temp,p[20];
+  int i,j,n,bt[20],pos,temp,p[20],wt[20],tat[20];
+  float awt=0,atat=0;
   printf("Enter Total Number of Processes:");
   scanf("%d",&n);
 
@@ -32,8 +33,29 @@ int main() {
     p[i] = p[pos];
     p[pos] = temp;
   }
+  //Calculating Waiting Time
+  wt[0] = 0;
+  for (i = 1; i < n; i++) {
+    wt[i] = 0;
+    for(j=0;j<i;j++)
+      wt[i] = wt[i]+bt[j];
+  }
+  printf("\nProcess\t Burst Time \t Waiting Time \t TurnAround Time");
+  //Calculating TurnAround Time
+  for(i=0;i<n;i++)
+  {
+    tat[i] = bt[i]+wt[i];
+    atat = atat+tat[i];
+    awt = awt+wt[i];
+    printf("\np[%d] \t\t %d \t\t %d \t\t %d",i+1,bt[i],wt[i],tat[i]);
+  }
+  awt = awt/n;
+  atat = atat/n;
+  printf("\nAverage Waiting Time:%.2f\n",awt);
+  printf("Average TurnAround Time:%.2f\n",atat);
 
   //...........................................
+
   return 0;
 
 }
