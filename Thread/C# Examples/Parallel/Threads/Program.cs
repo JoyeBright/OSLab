fbccb73cc931136f032349ae53516aabfc51f6d5
Aryan <aryanebrahimpour@gmail.com>
2018-04-12 01:26:37 +0430

    Initialize PLINQ Project

---
+++ Thread/C# Examples/Parallel/Threads/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Threads
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            StartAllThreads();
+
+        }
+
+        static void StartAllThreads()
+        {
+            foreach (var item in SharedInfo.Data)
+            {
+                new Thread(() => Print(item))
+                {
+                    IsBackground = true,
+                    Priority = ThreadPriority.AboveNormal
+                }.Start();
+            }
+        }
+
+        static void Print(int item)
+        {
+            Console.WriteLine($"Result of Sum for [0 to {item}] is: {item.SumFromZero()}");
+        }
+    }
+}
