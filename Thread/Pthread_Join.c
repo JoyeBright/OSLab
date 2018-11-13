a3eba9507925716335f499047a3940d80788e70f
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-11-13 21:10:35 +0330

    Revised version of thread library

--- Thread/Pthread_Join.c
+++ Thread/Pthread_Join.c
@@ -15,12 +15,15 @@ int main() {
   pthread_attr_t attr;
   pthread_attr_init(&attr);
   int data_passed = 5;
-  
+
   int rv = pthread_create(&tid, &attr, PrintHello, &data_passed);
   if(rv){
     printf("Error: pthread_create generates error %d\n",rv);
     return 0;
   }
+  // pthread_join is a functionality of pthread library which allows
+  // the originating(main) thread to wait for the completion of all its
+  // spawned thread's tasks
   pthread_join(tid, NULL);
   printf("The new thread id: %lld\n",(long long)tid);
   pthread_exit(NULL);
