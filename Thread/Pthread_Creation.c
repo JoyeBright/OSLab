9f9139ce398f3f75ec5a648de7dd8e5d8d679656
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-11-17 01:25:48 +0330

    RR added

--- Thread/Pthread_Creation.c
+++ Thread/Pthread_Creation.c
@@ -3,17 +3,26 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-void* PrintHello(void* data){
-  int *my_data = (int*)data; // Data gets by thread
-  int val = * my_data;
-  printf("Hello from new thread - got %d\n", val);
+long long sum = 0;
+
+void* sum_runner(void* data){
+  long long *my_data = (long long*)data; // Data gets by thread
+  long long val = * my_data;
+  for(long long i=0;i<=val;i++){
+    sum +=i;
+  }
   pthread_exit(NULL);
 }
-int main() {
+int main(int argc, char const *argv[]) {
+  if(argc>2){
+    printf("Usage: %s <num>\n",argv[0]);
+    return 0;
+  }
   int rv; // Return Value
   pthread_t thread_id; // Thread's ID (int)
-  int data_passed = 5; // Data passed to the new thread
+  long long data_passed = atoll(argv[1]); // Data passed to the new thread
 
   // Create a new thread that will execute 'PrintHello'
   /*
@@ -26,13 +35,15 @@ int main() {
      and that our new thread can return an arbitrary piece of data when it finishes.
      how do we pass our thread an arbitrary argument? we use the fourth arg to call.
   */
-  rv = pthread_create(&thread_id, NULL, PrintHello, &data_passed);
+  rv = pthread_create(&thread_id, NULL, sum_runner, &data_passed);
   if(rv)
   {
     printf("Error: return code from pthread_create is %d\n", rv);
     return (0);
   }
   printf("\nCreated new thread (%d) .... \n", (int)thread_id);
+  pthread_join(thread_id, NULL);
+  printf("\nsum: %lld\n", sum);
   // Terminate the thread
   pthread_exit(NULL);
 }
